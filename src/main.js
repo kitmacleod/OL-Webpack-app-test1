1e52587bc06c7294189e27d14b575b17b7d10c0b
Kit Macleod <kitmacleod@gmail.com>
2018-05-11 04:41:16 +0100

    Fixed draw current location

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import Map from 'ol/map';
 import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
 import XYZSource from 'ol/source/xyz';
+import proj from 'ol/proj';
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
 
 // Workshop code 
-new Map({
+const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
@@ -19,3 +24,20 @@ new Map({
     zoom: 2
   })
 });
+
+// Draw current location
+const position = new VectorSource();
+const vector = new VectorLayer({
+  source: position
+});
+map.addLayer(vector);
+
+
+
+// Geolocation functionality
+navigator.geolocation.getCurrentPosition(function(pos) {
+  const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
+  map.getView().animate({center: coords, zoom: 10});
+  position.addFeature(new Feature(new Point(coords)));
+});
+
